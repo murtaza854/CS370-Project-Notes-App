c09ada815002dd0f7fd4355dfecfca136f0ba05b
murtaza854 <murtazashafi11@gmail.com>
2020-12-11 01:46:01 +0500

    Murtaza 1, Khubabib 0

--- src/app/notecard/notecard.component.ts
+++ src/app/notecard/notecard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-notecard',
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notecard.component.scss']
 })
 export class NotecardComponent implements OnInit {
+  @Input("title") title: string;
+  @Input("body") body: string ="idiot idiot idiot idi otidioti diotidiotidiotidiotidio tidio tidiotidiotidiotidiot idiotidi otidiotidiotidiotidiotidiot";
+  // @ViewChild('truncator')
+  // truncator!: ElementRef<HTMLElement>;
+  // @ViewChild('bodyText')
+  // bodyText!: ElementRef<HTMLElement>;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    // let style = window.getComputedStyle(this.bodyText.nativeElement,null);
+    // let viewableHeight =parseInt(style.getPropertyValue('Height'),10);
+    // if (this.bodyText.nativeElement.scrollHeight >  viewableHeight) {
+    //   this.renderer.setStyle(this.truncator.nativeElement,'display','block');
+    // }
+    // else
+    // {
+
+    //   this.renderer.setStyle(this.truncator.nativeElement,'display','none');
+    // }
   }
 
 }
